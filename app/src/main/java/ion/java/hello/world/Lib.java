2642788c00297d24b7c7c6313db643b8448e76ae
Joshua Barr <jobarr@amazon.com>
2023-03-01 17:40:28 -0800

    Solved

--- app/src/main/java/ion/java/hello/world/Lib.java
+++ app/src/main/java/ion/java/hello/world/Lib.java
@@ -1,14 +1,37 @@
 package ion.java.hello.world;
 
+import com.amazon.ion.IonReader;
+import com.amazon.ion.IonType;
+import com.amazon.ion.IonWriter;
+import com.amazon.ion.system.IonBinaryWriterBuilder;
+import com.amazon.ion.system.IonReaderBuilder;
+import com.amazon.ion.system.IonTextWriterBuilder;
+import com.fasterxml.jackson.dataformat.ion.IonFactory;
+import com.fasterxml.jackson.dataformat.ion.IonObjectMapper;
+import com.fasterxml.jackson.dataformat.ion.IonParser;
+import ion.java.hello.world.App.Record;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Lib {
-    public static void processRecords(List<App.Record> records) {
+    public static void processRecords(List<Record> records) {
         if (records != null) {
             records.stream()
-                    .collect(Collectors.groupingBy((App.Record r) -> r.rank))
+                    .collect(Collectors.groupingBy((Record r) -> r.rank))
                     .values()
                     .forEach(l -> {
                         l.stream()
@@ -21,4 +44,5 @@ public class Lib {
             System.exit(1);
         }
     }
+
 }
